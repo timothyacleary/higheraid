6e353d5588c43057b5d01ddc8967c863da5b8077
Tim <tim@higheraid.com>
2013-03-24 02:11:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Higheraid::Application.config.secret_token = '7e36682eabd7db7524aa8cffcf180e8c0c1dd00a950b24158612b9b4f6eb2b1e59e3b01ab9ece03e2293a7928febfda42ddf67fcbf3cb89d19ca0da3474c9914'
